5adc1c16e219654af9af67073afb7b1d5730960c
MinaYasser100 <minayasser43@gmail.com>
2024-07-02 17:34:23 +0300

    implement button to remove the question widget from screen

--- lib/features/teacher/presentation/manager/add_new_question_cubit.dart
+++ lib/features/teacher/presentation/manager/add_new_question_cubit.dart
@@ -3,18 +3,35 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../view/widgets/custom_question_widget.dart';
 
-part 'teacher_states.dart';
+part 'add_new_question_states.dart';
 
 class AddNewQuestionCubit extends Cubit<AddNewQuestionStates> {
   AddNewQuestionCubit() : super(AddNewQuestionInitial()) {
-    emit(AddNewQuestionCubitLoaded([const CustomQuestionWidget()]));
+    emit(AddNewQuestionCubitLoaded([
+      const CustomQuestionWidget(
+        index: 0,
+      )
+    ]));
   }
 
   void addNewQuestion() {
     if (state is AddNewQuestionCubitLoaded) {
       final currentState = state as AddNewQuestionCubitLoaded;
       final newQuestions = List<Widget>.from(currentState.questions);
-      newQuestions.add(const CustomQuestionWidget());
+      newQuestions.add(CustomQuestionWidget(index: newQuestions.length));
+      emit(AddNewQuestionCubitLoaded(newQuestions));
+    }
+  }
+
+  void removeQuestion(int index) {
+    if (state is AddNewQuestionCubitLoaded) {
+      final currentState = state as AddNewQuestionCubitLoaded;
+      final newQuestions = List<Widget>.from(currentState.questions);
+      newQuestions.removeAt(index);
+      // Update indices
+      for (int i = 0; i < newQuestions.length; i++) {
+        newQuestions[i] = CustomQuestionWidget(index: i);
+      }
       emit(AddNewQuestionCubitLoaded(newQuestions));
     }
   }
--- lib/features/teacher/presentation/view/widgets/custom_question_widget.dart
+++ lib/features/teacher/presentation/view/widgets/custom_question_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quiz_app/core/constant/app_color.dart';
+import 'package:quiz_app/features/teacher/presentation/manager/add_new_question_cubit.dart';
 
 import 'custom_answer_position.dart';
 import 'custom_text_from_field.dart';
@@ -7,38 +9,80 @@ import 'custom_text_from_field.dart';
 class CustomQuestionWidget extends StatelessWidget {
   const CustomQuestionWidget({
     super.key,
+    required this.index,
   });
-
+  final int index;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: Colors.grey[200],
-        borderRadius: BorderRadius.circular(10),
-        border: Border.all(
-          color: AppColor.secondColor, // Border color
-          width: 1.0, // Border width
+    return Stack(
+      clipBehavior: Clip.none, // Allow overflow to show the CircleAvatar
+      children: [
+        Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.grey[100],
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(
+              color: AppColor.secondColor, // Border color
+              width: 1.0, // Border width
+            ),
+          ),
+          child: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Question'),
+                CustomTextFromField(),
+                SizedBox(
+                  height: 20,
+                ),
+                Text('Answer'),
+                CustomAnswerPosition(),
+                CustomAnswerPosition(),
+                CustomAnswerPosition(),
+                CustomAnswerPosition(
+                  isLast: true,
+                ),
+              ],
+            ),
+          ),
         ),
-      ),
-      child: const Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Question'),
-            CustomTextFromField(),
-            SizedBox(
-              height: 20,
-            ),
-            Text('Answer'),
-            CustomAnswerPosition(),
-            CustomAnswerPosition(),
-            CustomAnswerPosition(),
-            CustomAnswerPosition(
-              isLast: true,
-            ),
-          ],
+        if (index != 0)
+          CustomRemoveQuestionButton(
+            index: index,
+            onTap: () {
+              context.read<AddNewQuestionCubit>().removeQuestion(index);
+            },
+          ),
+      ],
+    );
+  }
+}
+
+class CustomRemoveQuestionButton extends StatelessWidget {
+  const CustomRemoveQuestionButton({
+    super.key,
+    required this.index,
+    this.onTap,
+  });
+
+  final int index;
+  final Function()? onTap;
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: -12,
+      right: -12,
+      child: GestureDetector(
+        onTap: onTap,
+        child: const CircleAvatar(
+          radius: 24,
+          backgroundColor: AppColor.secondColor,
+          child: Icon(
+            Icons.remove_circle_outline_rounded,
+            color: Colors.white,
+          ),
         ),
       ),
     );
