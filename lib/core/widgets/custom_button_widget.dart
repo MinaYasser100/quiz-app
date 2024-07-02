fbdda52d7adc4fcf167d7fbe598d79970d6762a2
MinaYasser100 <minayasser43@gmail.com>
2024-07-02 16:11:26 +0300

    add the buttons and flutter bloc

--- lib/core/widgets/custom_button_widget.dart
+++ lib/core/widgets/custom_button_widget.dart
@@ -8,18 +8,20 @@ class CustomButtonWidget extends StatelessWidget {
     super.key,
     required this.text,
     required this.onPressed,
+    this.buttonColor = AppColor.primaryColor,
   });
   final String text;
   final void Function() onPressed;
+  final Color buttonColor;
   @override
   Widget build(BuildContext context) {
     return Container(
       clipBehavior: Clip.antiAliasWithSaveLayer,
       height: 50,
       width: 200,
-      decoration: const BoxDecoration(
-        color: AppColor.primaryColor,
-        borderRadius: BorderRadius.only(
+      decoration: BoxDecoration(
+        color: buttonColor,
+        borderRadius: const BorderRadius.only(
           bottomLeft: Radius.circular(30),
           topRight: Radius.circular(30),
         ),
--- lib/features/teacher/presentation/view/widgets/teacher_body_view.dart
+++ lib/features/teacher/presentation/view/widgets/teacher_body_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/core/constant/app_color.dart';
+import 'package:quiz_app/core/widgets/custom_button_widget.dart';
 
 import 'custom_question_widget.dart';
 
@@ -7,11 +9,27 @@ class TeacherBodyView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
       child: Column(
         children: [
-          CustomQuestionWidget(),
+          const CustomQuestionWidget(),
+          const SizedBox(
+            height: 20,
+          ),
+          CustomButtonWidget(
+            text: 'Next',
+            buttonColor: AppColor.secondColor,
+            onPressed: () {},
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          CustomButtonWidget(
+            text: 'Finish',
+            buttonColor: AppColor.secondColor,
+            onPressed: () {},
+          ),
         ],
       ),
     );
