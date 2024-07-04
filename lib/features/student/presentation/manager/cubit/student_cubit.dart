fedbf0acc61be17eecf9144d631392914f5c797a
MinaYasser100 <minayasser43@gmail.com>
2024-07-04 23:59:48 +0300

    do get quiz questions method for student

--- lib/features/student/presentation/manager/cubit/student_cubit.dart
+++ lib/features/student/presentation/manager/cubit/student_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -5,6 +6,31 @@ part 'student_state.dart';
 
 class StudentCubit extends Cubit<StudentState> {
   StudentCubit() : super(StudentInitial());
-
-  Future<void> getQuizQuestions() async {}
+  List<Map<String, dynamic>>? questions;
+  Future<void> getQuizQuestions({required String quizCode}) async {
+    emit(StudentCubitGetQuizQuestionsLoading());
+    try {
+      DocumentSnapshot<Map<String, dynamic>> documentSnapshot =
+          await FirebaseFirestore.instance
+              .collection('questions')
+              .doc(quizCode)
+              .get();
+      if (documentSnapshot.exists) {
+        Map<String, dynamic>? data = documentSnapshot.data();
+        if (data != null) {
+          questions = List<Map<String, dynamic>>.from(data['questions']);
+          print(questions);
+          emit(StudentCubitGetQuizQuestionsSuccess());
+        } else {
+          emit(StudentCubitGetQuizQuestionsFailure(
+              error: 'No data found for the provided quiz code.'));
+        }
+      } else {
+        emit(StudentCubitGetQuizQuestionsFailure(
+            error: 'Quiz not found for the provided quiz code.'));
+      }
+    } catch (e) {
+      emit(StudentCubitGetQuizQuestionsFailure(error: e.toString()));
+    }
+  }
 }
--- lib/features/student/presentation/manager/cubit/student_state.dart
+++ lib/features/student/presentation/manager/cubit/student_state.dart
@@ -4,3 +4,13 @@ part of 'student_cubit.dart';
 sealed class StudentState {}
 
 final class StudentInitial extends StudentState {}
+
+final class StudentCubitGetQuizQuestionsLoading extends StudentState {}
+
+final class StudentCubitGetQuizQuestionsSuccess extends StudentState {}
+
+final class StudentCubitGetQuizQuestionsFailure extends StudentState {
+  final String error;
+
+  StudentCubitGetQuizQuestionsFailure({required this.error});
+}
